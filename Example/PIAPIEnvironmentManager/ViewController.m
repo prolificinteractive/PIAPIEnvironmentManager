e1a9003bb96f677a45a7b2ee688e24a95225c67b
Julio Rivera <julio@prolificinteractive.com>
2015-02-15 15:47:41 -0500

    update for new implementation

--- Example/PIAPIEnvironmentManager/ViewController.m
+++ Example/PIAPIEnvironmentManager/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "PIAPIEnvironmentManager.h"
+#import <PIAPIEnvironmentManager/PIAPIEnvironmentManager.h>
 
 @interface ViewController () <PIAPIEnvironmentManagerDelegate>
 
@@ -16,20 +16,9 @@
 @end
 
 @implementation ViewController
-//
-//- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event {
-//    [PIAPIEnvironmentManager sharedManager].delegate = self;
-//    [[PIAPIEnvironmentManager sharedManager] presentEnvironmentViewControllerInViewController:self
-//                                                                                     animated:YES
-//                                                                                   completion:nil];
-//}
-//
-//- (void)environmentManagerWillChangeEnvironment:(PIAPIEnvironmentType)environmentType {
-//#warning Clear session data
-//}
-//
-//- (IBAction)updateURLButtonPressed:(id)sender {
-//    self.currentURLLabel.text = [[PIAPIEnvironmentManager sharedManager].currentEnvironmentURL absoluteString];
-//}
+
+- (IBAction)updateURLButtonPressed:(id)sender {
+    self.currentURLLabel.text = [[PIAPIEnvironmentManager sharedManager].currentEnvironmentURL absoluteString];
+}
 
 @end
